89b9b19134eb7d5eaaccd73e7e2d71db39efc185
ec <ec@dlmail.co.uk>
2022-06-14 14:58:04 +0100

    Files for projects

---
+++ make_predict_azure_app.sh
@@ -0,0 +1,3 @@
+PORT=443
+echo "Port: $PORT"
+curl -d '{"CHAS":{"0":0},"RM":{"0":6.575},"TAX":{"0":296.0},"PTRATIO":{"0":15.3},"B":{"0":396.9},"LSTAT":{"0":4.98}}' -H "Content-Type: application/json" -X POST https://adnd-flask-ml.azurewebsites.net:443/predict
